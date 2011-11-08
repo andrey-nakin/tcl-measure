da656b0394f67ba0f309a868bd6f5fb9466b6107
Andrey Nakin <andrey.nakin@gmail.com>
2011-11-08 16:02:51 +0400

    modified:   measure/pkgIndex.tcl

--- measure/pkgIndex.tcl
+++ measure/pkgIndex.tcl
@@ -1,3 +1,4 @@
 package ifneeded measure::logger 0.1.0 [list source [file join $dir logger.tcl]]
 package ifneeded measure::parallel 0.1.0 [list source [file join $dir parallel.tcl]]
+package ifneeded measure::widget 0.1.0 [list source [file join $dir widget/widget.tcl]]
 
