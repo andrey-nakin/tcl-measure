de15891797556b0de08dcacb5f7d53efa12f021d
Andrey Nakin <andrey.nakin@gmail.com>
2011-12-09 22:33:25 +0400

    modified:   assemblies/assembly001/measure.tcl

--- assemblies/assembly001/measure.tcl
+++ assemblies/assembly001/measure.tcl
@@ -15,6 +15,7 @@ package require tclvisa
 package require measure::datafile
 package require measure::interop
 package require measure::sigma
+package require math::statistics
 
 ###############################################################################
 # Подпрограммы
@@ -30,64 +31,66 @@ proc setCurrent { curr } {
 }
 
 # Измеряет ток и напряжение на образце
-# Возвращает напряжение, погрешность в милливольтах, ток и погрешность в миллиамперах
+# Возвращает напряжение, погрешность в милливольтах, ток и погрешность в миллиамперах, сопротивление и погрешность в омах
 proc measureVoltage { } {
     global mm cmm measure
     
-	if { $measure(numberOfSamples) > 1 } {
-		# режим многократного измерения с подсчётом статистики
+	# запускаем измерение напряжения
+	scpi::cmd $mm "INIT"
 
-		# запускаем измерения напряжения
-		scpi::cmd $mm "INIT"
+	# запускаем измерение тока
+	scpi::cmd $cmm "INIT"
 
-		# запускаем измерения тока
-		scpi::cmd $cmm "INIT"
+	# ждём завершения измерения напряжения
+	scpi::query $mm "*OPC?"
+
+	# ждём завершения измерения тока
+	scpi::query $cmm "*OPC?"
+
+	# списки для хранения значений
+	set vs [list]
+	set cs [list]
+	set rs [list]
+
+	# цикл измерения
+	set n $measure(numberOfSamples)
+	for { set i 0 } { $i < $n } { incr i } {
+	    # проверим, не нажата ли кнопка остановки
+	    measure::interop::checkTerminated
+
+		# считываем напряжение
+		set v [expr abs([scpi::query $mm "DATA:REMOVE? 1"])]
+		lappend vs $v
+
+		# считываем ток
+		set c [expr abs([scpi::query $cmm "DATA:REMOVE? 1"])]
+		lappend cs $c
+
+		# вычисляем сопротивление
+		lappend rs [expr $v / $c]
 		
-		# цикл ожидания конца измерений
-		for { set i 0 } { $i < $measure(numberOfSamples) } { incr i } {
-		    # проверим, не нажата ли кнопка остановки
-		    measure::interop::checkTerminated
-		    
-			# немного подождём
-			after 1000
-		
-			# считываесм число накопленных в статистике измерений
-			set n1 [scpi::query $mm "CALCULATE:AVERAGE:COUNT?"]
-			set n2 [scpi::query $cmm "CALCULATE:AVERAGE:COUNT?"]
-			if { $n1 >= $measure(numberOfSamples) && $n2 >= $measure(numberOfSamples) } {
-				# считываем среднее и станд. отклонение
-				set cavg [scpi::query $cmm "CALCULATE:AVERAGE:AVERAGE?"]
-				set cstd [scpi::query $cmm "CALCULATE:AVERAGE:SDEVIATION?"]
-				
-				set avg [scpi::query $mm "CALCULATE:AVERAGE:AVERAGE?"]
-				set std [scpi::query $mm "CALCULATE:AVERAGE:SDEVIATION?"]
-
-				# Добавляем систематическую погрешность
-				# и возвращаем результат измерений, переведённый в милливольты и милливольты
-				set cerr [measure::sigma::add $cstd [hardware::agilent::mm34410a::dciSystematicError $cavg]]
-				set err [measure::sigma::add $std [hardware::agilent::mm34410a::dcvSystematicError $avg]]
-				return [list [expr 1000.0 * abs($avg)] [expr 1000.0 * $err] [expr 1000.0 * abs($cavg)] [expr 1000.0 * $cerr] ]
-			}
-			
-			# выведем прогресс измерения на индикаторы мультиметров
-			scpi::cmd $mm "DISPLAY:WINDOW2:TEXT:DATA \"[format %0.0f [expr 100.0 * $n1 / $measure(numberOfSamples)]]% measured\""
-			scpi::cmd $cmm "DISPLAY:WINDOW2:TEXT:DATA \"[format %0.0f [expr 100.0 * $n2 / $measure(numberOfSamples)]]% measured\""
-		}
-
-		error "Таймаут ожидания измерения напряжения"
-	} else {
-		# режим однократного измерения
-
-		# Считываем напряжение
-		set cres [scpi::query $cmm "READ?"]
-		set res [scpi::query $mm "READ?"]
-
-		# Добавляем систематическую погрешность
-		# и возвращаем результат измерений, переведённый в милливольты
-		set cerr [hardware::agilent::mm34410a::dciSystematicError $cres]
-        set verr [hardware::agilent::mm34410a::dcvSystematicError $res]  
-		return [list [expr 1000.0 * abs($res)] [expr 1000.0 * $verr] [expr 1000.0 * abs($cres)] [expr 1000.0 * $cerr] ]
+		# выведем прогресс измерения на индикаторы мультиметров
+		#scpi::cmd $mm "DISPLAY:WINDOW2:TEXT:DATA \"[format %0.0f [expr 100.0 * ($i + 1) / $n]]% measured\""
+		#scpi::cmd $cmm "DISPLAY:WINDOW2:TEXT:DATA \"[format %0.0f [expr 100.0 * ($i + 1) / $n]]% measured\""
 	}
+
+	# вычисляем средние значения и сигмы
+	set v [math::statistics::mean $vs]; set sv [math::statistics::stdev $vs]
+	set c [math::statistics::mean $cs]; set sc [math::statistics::stdev $cs]
+	set r [math::statistics::mean $rs]; set sr [math::statistics::stdev $rs]
+
+	# определяем инструментальную погрешность
+	set vErr [hardware::agilent::mm34410a::dcvSystematicError $v]
+	set cErr [hardware::agilent::mm34410a::dciSystematicError $c]
+	set rErr [measure::sigma::div $v $vErr $c $cErr]
+
+	# суммируем инструментальную и измерительную погрешности
+	set vErr [measure::sigma::add $vErr $sv]
+	set cErr [measure::sigma::add $cErr $sc]
+	set rErr [measure::sigma::add $rErr $sr]
+
+	# возвращаем результат измерений, переведённый в милливольты и милливольты
+	return [list [expr 1000.0 * $v] [expr 1000.0 * $vErr] [expr 1000.0 * $c] [expr 1000.0 * $cErr] $r $rErr]
 }
 
 # Устанавливает положение переключателей полярности
@@ -123,6 +126,10 @@ proc setupMM {} {
 
     # Иниализируем и опрашиваем ММ
     hardware::agilent::mm34410a::init $mm
+
+	if { [scpi::query $mm "ROUTE:TERMINALS?"] != "FRON" } {
+		error "Turn Front/Rear switch of voltmeter to Front"
+	}
 
 	# включаем режим измерения пост. напряжения
 	scpi::cmd $mm "CONFIGURE:VOLTAGE:DC AUTO"
@@ -171,6 +178,10 @@ proc setupCMM {} {
 
     # Иниализируем и опрашиваем ММ
     hardware::agilent::mm34410a::init $cmm
+
+	if { [scpi::query $cmm "ROUTE:TERMINALS?"] != "FRON" } {
+		error "Turn Front/Rear switch of ampermeter to Front"
+	}
 
 	# включаем режим измерения пост. тока
 	scpi::cmd $cmm "CONFIGURE:CURRENT:DC AUTO"
@@ -283,10 +294,9 @@ for { set curr $measure(startCurrent) } { $curr <= $measure(endCurrent) + 0.1 * 
 	# выставляем ток на ИП
 	setCurrent $curr
 
-	set vsum 0.0
-	set svsum 0.0
-	set isum 0.0
-	set sisum 0.0
+	set vs [list]; set svs [list]
+	set cs [list]; set scs [list]
+	set rs [list]; set srs [list]
 
 	# Пробегаем по переполюсовкам
 	set nc [llength $connectors]
@@ -302,28 +312,26 @@ for { set curr $measure(startCurrent) } { $curr <= $measure(endCurrent) + 0.1 * 
 		# Измеряем напряжение
 		set res [measureVoltage]
 
-		# Накапливаем сумму напряжения и погрешности
-		lassign $res v sv i si
-		set vsum [expr $vsum + $v]
-		set svsum [expr $svsum + $sv]
-		set isum [expr $isum + $i]
-		set sisum [expr $sisum + $si]
+		# Накапливаем суммы
+		lassign $res v sv c sc r sr
+		lappend vs $v; lappend svs $vs
+		lappend cs $c; lappend scs $cs
+		lappend rs $r; lappend srs $rs
           
         # Выводим результаты в окно программы
-    	measure::interop::setVar runtime(current) [format "%0.9g \u2213 %0.2g" $i $si]
+    	measure::interop::setVar runtime(current) [format "%0.9g \u2213 %0.2g" $c $sc]
     	measure::interop::setVar runtime(voltage) [format "%0.9g \u2213 %0.2g" $v $sv]
-    	measure::interop::setVar runtime(resistance) [format "%0.9g \u2213 %0.2g" [expr $v / $i] [measure::sigma::div $v $sv $i $si]]
-    	measure::interop::setVar runtime(power) [format "%0.3g" [expr 0.001 * $i * $v]]
+    	measure::interop::setVar runtime(resistance) [format "%0.9g \u2213 %0.2g" $r $sr]
+    	measure::interop::setVar runtime(power) [format "%0.3g" [expr 0.001 * $c * $v]]
 	}
 
-	# Вычисляем средние значения тока, напряжения и погрешностей
-	set v [expr $vsum / $nc]
-	set sv [expr $svsum / $nc]
-	set i [expr $isum / $nc]
-	set si [expr $sisum / $nc]
+	# Вычисляем средние значения
+	set c [math::statistics::mean $cs]; set sc [math::statistics::mean $scs]
+	set v [math::statistics::mean $vs]; set sv [math::statistics::mean $svs]
+	set r [math::statistics::mean $rs]; set sr [math::statistics::mean $srs]
 
     # Выводим результаты в результирующий файл
-	measure::datafile::write $measure(fileName) $measure(fileFormat) [list $i $si $v $sv [expr $v / $i] [measure::sigma::div $v $sv $i $si] ]
+	measure::datafile::write $measure(fileName) $measure(fileFormat) [list $c $sc $v $sv $r $sr]
 }
 
 ###############################################################################
