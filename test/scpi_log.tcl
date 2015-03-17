006d9161128795778e46a43fe89330a991bc392f
Andrey Nakin <andrey.nakin@gmail.com>
2015-03-17 13:46:17 +0300

    new file:   scpi_log.tcl 	new file:   scpi_stress.tcl

---
+++ test/scpi_log.tcl
@@ -0,0 +1,63 @@
+#!/usr/bin/tclsh
+
+# Usage: <scpi_log> <mode>
+# Example: COM1 9600,n,8,1
+
+set PORT ASRL2::INSTR
+set MODE "9600,n,8,1"
+
+set port $PORT
+if { [llength $argv] > 0 } {
+	set port [lindex $argv 0]
+}
+
+set mode $MODE
+if { [llength $argv] > 1 } {
+	set mode [lindex $argv 1]
+}
+
+if { [string first INSTR $port] < 0 } {
+    if {[catch {set fd [open $port r+]} err]} {
+    	puts $err
+    	exit
+    }
+} else {
+    package require tclvisa
+    set rm [visa::open-default-rm]
+    if {[catch {set fd [visa::open $rm $port]} err]} {
+    	puts $err
+    	exit
+    }
+}
+
+if {[catch {fconfigure $fd -blocking 1 -mode $mode -buffering line -encoding binary -translation binary} err]} {
+	close $fd
+	puts $err
+	exit
+}
+
+if {[catch {set log [open scpi.log a]} err]} {
+	puts $err
+	exit
+}
+
+puts $log "*** NEW SESSION STARTED [clock format [clock seconds] -format {%Y-%m-%d %H:%M:%S}] ***"
+puts "Type commands, exit to terminate"
+
+while { 1 } {
+    set cmd [gets stdin]
+    if { $cmd == "exit" } {
+        break
+    }
+    puts $log "[clock format [clock seconds] -format {%H:%M:%S}] > $cmd"
+    puts $fd $cmd
+    
+    if { [string first ? $cmd] >= 0 } {
+    	set s [gets $fd]
+    	puts $s
+    	puts $log "[clock format [clock seconds] -format {%H:%M:%S}] < $s"
+    }
+}
+
+catch {close $fd}
+
---
+++ test/scpi_stress.tcl
@@ -0,0 +1,70 @@
+#!/usr/bin/tclsh
+
+# Usage: <scpi_log> <mode>
+# Example: COM1 9600,n,8,1
+
+set PORT ASRL2::INSTR
+set MODE "9600,n,8,1"
+
+set port $PORT
+if { [llength $argv] > 0 } {
+	set port [lindex $argv 0]
+}
+
+set mode $MODE
+if { [llength $argv] > 1 } {
+	set mode [lindex $argv 1]
+}
+
+if { [string first INSTR $port] < 0 } {
+    if {[catch {set fd [open $port r+]} err]} {
+    	puts $err
+    	exit
+    }
+} else {
+    package require tclvisa
+    set rm [visa::open-default-rm]
+    if {[catch {set fd [visa::open $rm $port]} err]} {
+    	puts $err
+    	exit
+    }
+}
+
+if {[catch {fconfigure $fd -blocking 1 -mode $mode -buffering line -encoding binary -translation binary -timeout 30000} err]} {
+	close $fd
+	puts $err
+	exit
+}
+
+after 1000
+puts "Type Ctrl-C to exit"
+
+puts $fd "CONF:VOLT 1"
+puts $fd "SAMPLE:COUNT 3"
+
+while { 1 } {
+    #puts $fd "INIT"
+    #puts $fd "FETCH?"
+    #puts "FETCH = [gets $fd]"
+    puts $fd "READ?"
+    puts "READ = [gets $fd]"
+    puts $fd "SYST:ERR?"
+    set err [gets $fd]
+    puts "err = $err"
+    if { $err == "" } {
+        while { $err == "" } {
+            puts $fd "*IDN?"
+            set err [gets $fd]
+            puts "err = $err"
+        }
+        continue;
+    }
+    set err [lindex [split $err ,] 0]
+    if { $err != 0 } {
+        puts "ERROR $err";
+        break;
+    }
+}
+
+catch {close $fd}
+
